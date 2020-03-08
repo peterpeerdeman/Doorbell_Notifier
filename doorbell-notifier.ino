ec22b93e41b6a5de9d151c60f28b9cf5767fd320
Peter Peerdeman <peter@lifely.nl>
2020-03-08 12:34:09 +0100

    feat(environment): changed doorbell-notifier code to support single doorbell (my frontdoor) and work with homemade analog optocoupler (led and photoresistor)

--- doorbell-notifier.ino
+++ doorbell-notifier.ino
@@ -1,47 +1,32 @@
 #define FRONT_DOOR A0
-#define BACK_DOOR A1
 String FD_Last_Ring = "Unknown";
-String BD_Last_Ring = "Unknown";
-bool FD_Reading;
-bool BD_Reading;
+int FD_Reading;
 int FD_Counter = 0;
-int BD_Counter = 0;
 int FD_Max = 0;
-int BD_Max = 0;
+int Counter_Max = 0;
 SYSTEM_THREAD(ENABLED);
 
 void setup() {
     pinMode(FRONT_DOOR, INPUT);
-    pinMode(BACK_DOOR, INPUT);
     Particle.variable("FD_Last_Ring", FD_Last_Ring);
-    Particle.variable("BD_Last_Ring", BD_Last_Ring);
     Particle.variable("FD_Max", FD_Max);
-    Particle.variable("BD_Max", BD_Max);
-    Time.zone(-5);
+    Particle.variable("FD_Reading", FD_Reading);
+    Time.zone(+1);
 }
 
 void loop() {
-    FD_Reading = digitalRead(FRONT_DOOR);
-    BD_Reading = digitalRead(BACK_DOOR);
+    FD_Reading = analogRead(FRONT_DOOR);
     
     //If statements to increment the counter and determine if a new max value has been set
-    if (FD_Reading == false) {
+    if (FD_Reading > 20) {
         FD_Counter += 1;
     }
     else {
         FD_Counter = 0;
     }
+    
     if (FD_Counter > FD_Max) {
         FD_Max = FD_Counter;
-    }
-    if (BD_Reading == false) {
-        BD_Counter += 1;
-    }
-    else {
-        BD_Counter = 0;
-    }
-    if (BD_Counter > BD_Max) {
-        BD_Max = BD_Counter;
     }
     
     /*
@@ -55,12 +40,5 @@ void loop() {
         delay(3000); //A delay to allow for the doorbell to stop being pushed, to avoid multiple publishes for a single doorbell push
         FD_Counter = 0;
         FD_Max = 0;
-    }
-    if (BD_Counter > 400) {
-        Particle.publish("Back_Door", "Ring", 60, PRIVATE);
-        BD_Last_Ring = Time.timeStr();
-        delay(3000); //A delay to allow for the doorbell to stop being pushed, to avoid multiple publishes for a single doorbell push
-        BD_Counter = 0;
-        BD_Max = 0;
     }
 }
